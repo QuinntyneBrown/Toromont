b2b3925f3e35b8b19516f510f6fa349c429ad853
Quinntyne Brown <quinntynebrown@gmail.com>
2026-04-03 02:40:10 -0400

    auto-sync 2026-04-03T02:40:10-04:00

--- src/backend/IronvaleFleetHub.Functions/Functions/TelemetryIngestionFunction.cs
+++ src/backend/IronvaleFleetHub.Functions/Functions/TelemetryIngestionFunction.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using IronvaleFleetHub.Functions.Models;
 using IronvaleFleetHub.Functions.Services;
+using IronvaleFleetHub.Telemetry;
 
 namespace IronvaleFleetHub.Functions.Functions;
 
